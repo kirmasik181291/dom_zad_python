7b05cbf2c5c751c1809b87177412b786364a8517
kirmasik181291 <121875617+kirmasik181291@users.noreply.github.com>
2023-01-13 18:56:17 +0300

    Update task_2.py

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,7 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+with open("text_2.txt", "r", encoding= 'utf-8') as f_obj:
+    useful = [f'{count}. {line.strip()} - {len(line.split())} слов'
+              for count, line in enumerate(f_obj, 1)]
+    print(*useful, f"всего строк - {len(useful)}.", sep="\n")

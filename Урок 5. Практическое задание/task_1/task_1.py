390e8a420047870811b9c573708b0b366c2c08ab
kirmasik181291 <121875617+kirmasik181291@users.noreply.github.com>
2023-01-13 18:35:17 +0300

    Update task_1.py

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,10 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+with open("text_1.txt", 'w', encoding='utf-8') as f:
+    while True:
+        line = input('input new string or empty string to done:')
+        if not line:
+            break
+        #f.write(f"{line}\n")
+        print(line, file=f)
